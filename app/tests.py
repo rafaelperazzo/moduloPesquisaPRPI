282cf2609e4f1da240b194792ed7f9b980239da2
Rafael Perazzo <rafaelperazzo@gmail.com>
2025-04-09 19:10:20 -0300

    fix: corrigido os testes para simular diferentes formas de incluir avaliadores

--- app/tests.py
+++ app/tests.py
@@ -36,7 +36,7 @@ def test_cadastrar_projeto(client):
             "inovacao": "1",
             "justificativa": "Justificativa",
             "cpf": "02675287440",
-            "titulo": "O TITULO DO PROJETO PARA TESTES AUTOMATIZADOS",
+            "titulo": "(1)O TITULO DO PROJETO PARA TESTES AUTOMATIZADOS",
             "validade": "3",
             "palavras_chave": "palavras chave",
             "descricao_resumida": "Descrição resumida",
@@ -77,7 +77,7 @@ def test_cadastrar_projeto_sem_avaliadores(client):
             "inovacao": "1",
             "justificativa": "Justificativa",
             "cpf": "02675287440",
-            "titulo": "O TITULO DO PROJETO PARA TESTES AUTOMATIZADOS",
+            "titulo": "(2)O TITULO DO PROJETO PARA TESTES AUTOMATIZADOS",
             "validade": "3",
             "palavras_chave": "palavras chave",
             "descricao_resumida": "Descrição resumida",
@@ -90,9 +90,6 @@ def test_cadastrar_projeto_sem_avaliadores(client):
             "arquivo_plano2": open("/app/teste.pdf","rb"),
             "arquivo_plano3": open("/app/teste.pdf","rb"),
             "arquivo_comprovantes": open("/app/teste.pdf","rb"),
-            "avaliador1_email": "teste@123.com",
-            "avaliador2_email": "teste@456.com",
-            "avaliador3_email": "teste@789.com",
     }
     response = client.post("/cadastrarProjeto", 
                            data=data,follow_redirects=True,content_type='multipart/form-data')
@@ -102,8 +99,48 @@ def test_cadastrar_projeto_sem_avaliadores(client):
     assert response.status_code == 200
     assert b"Propostas submetidas" in response.data
     assert b"O TITULO DO PROJETO PARA TESTES AUTOMATIZADOS" in response.data
-    assert b"teste@123.com" in response.data
 
+def test_cadastrar_projeto_sem_avaliadores_vazio(client):
+    """Test the cadastrar_projeto route."""
+    data={
+            "tipo": "40",
+            "nome": "nome",
+            "categoria_projeto": "1",
+            "siape": "1570709123",
+            "email": "rafaelperazzo@gmail.com",
+            "ua": "CCT",
+            "area_capes": "CIENCIA_DA_COMPUTACAO",
+            "grande_area": "Engenharias",
+            "grupo": "Grupo de pesquisa",
+            "ods_projeto": "01",
+            "inovacao": "1",
+            "justificativa": "Justificativa",
+            "cpf": "02675287440",
+            "titulo": "(3)O TITULO DO PROJETO PARA TESTES AUTOMATIZADOS",
+            "validade": "3",
+            "palavras_chave": "palavras chave",
+            "descricao_resumida": "Descrição resumida",
+            "numero_bolsas": "1",
+            "transporte": "1",
+            "inicio": "2023-10-01",
+            "fim": "2024-10-01",
+            "arquivo_projeto": open("/app/teste.pdf","rb"),
+            "arquivo_plano1": open("/app/teste.pdf","rb"),
+            "arquivo_plano2": open("/app/teste.pdf","rb"),
+            "arquivo_plano3": open("/app/teste.pdf","rb"),
+            "arquivo_comprovantes": open("/app/teste.pdf","rb"),
+            "avaliador1_email": "",
+            "avaliador2_email": "",
+            "avaliador3_email": "",
+    }
+    response = client.post("/cadastrarProjeto", 
+                           data=data,follow_redirects=True,content_type='multipart/form-data')
+    assert response.status_code == 200
+    assert b"FECHADA COM SEGU" in response.data
+    response = client.get("/editalProjeto?edital=40")
+    assert response.status_code == 200
+    assert b"Propostas submetidas" in response.data
+    assert b"O TITULO DO PROJETO PARA TESTES AUTOMATIZADOS" in response.data
 
 def test_edital_projeto(client):
     response = client.get("/editalProjeto?edital=40")

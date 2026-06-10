644cefc05a85b319914d043f7326224311469142
Rafael Perazzo <rafaelperazzo@gmail.com>
2026-06-10 09:21:28 -0300

    fix: Aceita . no final do email, e remove

--- app/pesquisa.py
+++ app/pesquisa.py
@@ -1464,7 +1464,7 @@ def inserirAvaliador():
     if request.method == "POST":
         token = id_generator(40)
         idProjeto = int(request.form['txtProjeto'])
-        avaliador1_email = str(request.form['txtEmail']).strip()
+        avaliador1_email = str(request.form['txtEmail']).strip().rstrip('.')
         if not re.match(r'^[^@\s]+@[^@\s]+\.[^@\s.]+$', avaliador1_email):
             return("E-mail inválido.")
         consulta_verificacao = """
